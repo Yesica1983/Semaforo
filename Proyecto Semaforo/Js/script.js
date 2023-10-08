e6c5eaa0e1acaf7c8e3d3d40f17de3368fdde4e9
yesica1983 <yesisofra22@outlook.com>
2023-10-08 13:53:37 -0300

    Mi primer pory semaforo

---
+++ Proyecto Semaforo/Js/script.js
@@ -0,0 +1,12 @@
+const $lucesDelCirculo = document.querySelectorAll('.luces-circulo');
+let contadorDeLuz = 0;
+
+const mostrarLuz = () =>{
+$lucesDelCirculo[contadorDeLuz].className ='luces-circulo';
+contadorDeLuz++; 
+
+if(contadorDeLuz >2) contadorDeLuz = 0;
+const luzActual = $lucesDelCirculo[contadorDeLuz];
+luzActual.classList.add(luzActual.getAttribute('color'))
+}
+setInterval(mostrarLuz,1000)
